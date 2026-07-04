24695c7c152ea8699ad3c35e0b347f8dbe8c997c
Lionel <gonzalezlionel7@gmail.com>
2026-07-04 20:41:09 -0300

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,7 @@
+from pelicula import Pelicula
+
+peliculas = [
+    Pelicula("Intensamente 2", "18:00", 50),
+    Pelicula("Deadpool & Wolverine", "20:00", 60),
+    Pelicula("Mi Villano Favorito 4", "22:00", 40)
+]
