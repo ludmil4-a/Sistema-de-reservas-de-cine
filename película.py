ba94b85f903af04ec12012c507adfb066741e296
Ludmila <122182756+ludmil4-a@users.noreply.github.com>
2026-07-05 14:48:49 -0300

    Add precio attribute to Pelicula class constructor

--- película.py
+++ película.py
@@ -1,9 +1,11 @@
 class Pelicula:
-    def __init__(self, nombre, horario, capacidad):
+    def __init__(self, nombre, horario, precio, capacidad):
         self.nombre = nombre
         self.horario = horario
+        self.precio = precio
         self.capacidad = capacidad
         self.entradas_vendidas = 0
+        self.recaudacion = 0 
 
     def lugares_disponibles(self):
         return self.capacidad - self.entradas_vendidas
