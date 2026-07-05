b7ea76f36cf37a6e79bd1f07eaea7490176052c5
Ludmila <122182756+ludmil4-a@users.noreply.github.com>
2026-07-05 20:08:53 -0300

    Update reserva.py

--- reserva.py
+++ reserva.py
@@ -1,39 +1,44 @@
 class Reserva:
 
-    PRECIO = 3000
-
     def __init__(self, pelicula, cantidad, medio_pago):
         self.pelicula = pelicula
         self.cantidad = cantidad
         self.medio_pago = medio_pago
+        self.descuento = 0
+        self.medio = ""
 
     def calcular_total(self):
 
-        subtotal = self.cantidad * self.PRECIO
+        subtotal = self.cantidad * self.pelicula.precio
 
         if self.medio_pago == 1:
-            descuento = 0.05
-            medio = "Efectivo"
+            self.descuento = 0.20
+            self.medio = "Efectivo (20%)"
 
         elif self.medio_pago == 2:
-            descuento = 0.10
-            medio = "Tarjeta de Débito"
+            self.descuento = 0.15
+            self.medio = "Tarjeta de Débito (15%)"
 
         elif self.medio_pago == 3:
-            descuento = 0.15
-            medio = "Tarjeta de Crédito"
+            self.descuento = 0.10
+            self.medio = "Tarjeta de Crédito (10%)"
 
         elif self.medio_pago == 4:
-            descuento = 0.20
-            medio = "Mercado Pago"
+            self.descuento = 0.05
+            self.medio = "Mercado Pago (5%)"
 
         else:
             descuento = 0
             medio = "Sin descuento"
 
-        total = subtotal - (subtotal * descuento)
+        total = subtotal - (subtotal * self.descuento)
 
-        print(f"\nMedio de pago: {medio}")
-        print(f"Descuento aplicado: {descuento * 100:.0f}%")
+        print(f"\nMedio de pago: {self.medio}")
+        print(f"Descuento aplicado: {self.descuento * 100:.0f}%")
 
         return total
+
+    def generar_asientos(self):
+
+        inicio = self.pelicula.entradas_vendidas - self.cantidad + 1
+        return list(range(inicio, inicio + self.cantidad))
