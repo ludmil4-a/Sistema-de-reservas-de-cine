d4fb890aa3beea58ddad44a5f4ea2782c635790f
Lionel <gonzalezlionel7@gmail.com>
2026-07-06 14:08:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,25 @@
 from cine import Cine
 from datos import peliculas
-from estadisticas import Estadisticas
 
 cine = Cine(peliculas)
 
 while True:
 
-    print("\n===== CINE =====")
+    print("\n===== SISTEMA CINE =====")
     print("1. Reservar entradas")
-    print("2. Ver estadísticas")
+    print("2. Ver ventas por función")
     print("3. Salir")
 
-    opcion = input("Seleccione una opción: ")
+    opcion = input("Opción: ")
 
     if opcion == "1":
         cine.reservar()
 
     elif opcion == "2":
-
-        print("\n----- Estadísticas -----")
-
-        print(
-            "Película más elegida:",
-            Estadisticas.pelicula_mas_vista(peliculas).nombre
-        )
-
-        print(
-            "Horario con mayor demanda:",
-            Estadisticas.horario_mas_demandado(peliculas)
-        )
-
-        print(
-            "Total de entradas vendidas:",
-            Estadisticas.total_entradas(peliculas)
-        )
+        cine.total_ventas_funcion()
 
     elif opcion == "3":
-        print("Hasta luego.")
+        print("Saliendo...")
         break
 
     else:
